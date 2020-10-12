3f0fba57fe11fd8921a0903f23cce27818b8b1a9
emaannnnnnnn5 <69813086+emaannnnnnnn5@users.noreply.github.com>
2020-10-12 13:21:57 -0400

    Add files via upload

---
+++ Particle.js
@@ -0,0 +1,28 @@
+class Particle {
+    constructor (x, y, r){
+        var options = {
+            restitution:0.4
+        }
+        this.r=r
+
+
+        this.body = Bodies.cricle(x, y, this.r, options);
+        this.color = color(random(0,255), random(0,255), random(0,255));
+        World.add(world, this.body);
+
+    }
+    display(){
+        var pos = this.body.psoition;
+        var angle = this.body.angle;
+
+        push();
+        trnaslate(pos.x, pos.y);
+        rotate(angle);
+        //imageMode(CENTER);
+        noStroke();
+        fill(this.color)
+        ellipseMode(RADIUS);
+        ellipse(0, 0, this.r, this.r);
+        pop();
+    }
+}
